100faaa3063b806b6a9227be52cbff04960537eb
Naum Azeredo <naumazeredo@gmail.com>
2016-08-26 03:54:18 -0300

    Updated background example

--- sketches/example_background.c
+++ sketches/example_background.c
@@ -1,12 +1,22 @@
 #include "sketch.h"
 
+#include <stdio.h>
+
 int r = 0, g = 0, b = 0;
 
 void setup() {
+  textSize(12);
 }
 
 void draw() {
   background(r, g, b);
+  text("Red:   press Q or A", 20, 20);
+  text("Green: press W or S", 20, 40);
+  text("Blue:  press E or D", 20, 60);
+
+  char buffer[256];
+  sprintf(buffer, "Color: #%02X%02X%02X", r, g, b);
+  text(buffer, 20, 80);
 
   if (keyPressed(KEY_Q)) r++;
   if (keyPressed(KEY_A)) r--;
